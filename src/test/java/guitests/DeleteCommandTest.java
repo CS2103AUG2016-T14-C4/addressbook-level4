647e39413b329d4bd8ce3d5cd09f633db5dd8284
AHaliq <liqo12@hotmail.com>
2016-10-13 20:15:43 +0800

    added test for string name matching

--- src/test/java/guitests/DeleteCommandTest.java
+++ src/test/java/guitests/DeleteCommandTest.java
@@ -3,6 +3,7 @@ package guitests;
 import org.junit.Test;
 import seedu.address.testutil.TestTask;
 import seedu.address.testutil.TestUtil;
+import seedu.address.testutil.TypicalTestTasks;
 
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.DeleteCommand.MESSAGE_DELETE_TASK_SUCCESS;
@@ -12,8 +13,13 @@ public class DeleteCommandTest extends AddressBookGuiTest {
     @Test
     public void delete() {
 
-        //delete the first in the list
         TestTask[] currentList = td.getTypicalTasks();
+
+        // delete a task with specific name
+        assertDeleteSuccess(TypicalTestTasks.fiona.getName().taskName, currentList);
+        currentList = TestUtil.removeTasksFromList(currentList, TypicalTestTasks.fiona);
+
+        //delete the first in the list
         int targetIndex = 1;
         assertDeleteSuccess(targetIndex, currentList);
 
@@ -43,6 +49,44 @@ public class DeleteCommandTest extends AddressBookGuiTest {
         TestTask[] expectedRemainder = TestUtil.removeTaskFromList(currentList, targetIndexOneIndexed);
 
         commandBox.runCommand("delete " + targetIndexOneIndexed);
+
+        //confirm the list now contains all previous tasks except the deleted task
+        assertTrue(taskListPanel.isListMatching(expectedRemainder));
+
+        //confirm the result message is correct
+        assertResultMessage(String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDelete));
+    }
+
+    /**
+     * Runs the delete command to delete the task with the specified targetName and confirms the result is correct.
+     * @param targetName e.g. to delete the task named "do work", "do work" should be given as targetName
+     * @param currentList A copy of the current list of tasks (before deletion)
+     */
+    private void assertDeleteSuccess(String targetName, final TestTask[] currentList) {
+        /** flag to determine if targetName matches multiple entries */
+        boolean multiMatch = false;
+        /** task object that matches targetName*/
+        TestTask taskToDelete = null;
+
+        // determine if multi match and search for task matching with targetName
+        for(TestTask tt: currentList) {
+            if(tt.getName().taskName.contentEquals(targetName)) {
+                if(taskToDelete == null) {
+                    taskToDelete = tt;
+                } else {
+                    multiMatch = true;
+                    break;
+                }
+            }
+        }
+        System.out.println(taskToDelete);
+        // generate expectedRemainder
+        TestTask[] expectedRemainder = multiMatch ?
+                currentList :
+                TestUtil.removeTasksFromList(currentList, taskToDelete);
+
+        // run deletion command
+        commandBox.runCommand("delete " + targetName);
 
         //confirm the list now contains all previous tasks except the deleted task
         assertTrue(taskListPanel.isListMatching(expectedRemainder));
