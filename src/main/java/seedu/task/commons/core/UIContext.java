9cc13c9f5a0eddf62eb92b428856905f3133a753
Syed Abdullah <syed@syedabdullah.net>
2016-11-02 21:22:49 +0800

    Created common class to represent the UI context enum

---
+++ src/main/java/seedu/task/commons/core/UIContext.java
@@ -0,0 +1,19 @@
+//@@author A0141052Y
+package seedu.task.commons.core;
+
+/**
+ * Provides static methods for the context of the current UI
+ * @author Syed Abdullah
+ *
+ */
+public class UIContext {
+    /**
+     * The type of UI context being referenced
+     * @author Syed Abdullah
+     *
+     */
+    public enum Type {
+        RESULT_PAGE,
+        WEB_BROWSER
+    }
+}
